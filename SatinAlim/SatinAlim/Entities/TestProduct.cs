9445d943dcafd8687b7cd5ce7542d5c50857a56a
Fatih KESİK <fatihkesik@usta.com.tr>
2024-07-12 17:54:41 +0300

    initial

---
+++ SatinAlim/SatinAlim/Entities/TestProduct.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SatinAlim.Entities
+{
+    public class TestProduct
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int TestProductID { get; set; }
+    }
+}
---
+++ SatinAlim/SatinAlim/Migrations/20240712140555_deneme3.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SatinAlim.Migrations
+{
+    /// <inheritdoc />
+    public partial class deneme3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "satinAlma");
+
+            migrationBuilder.RenameTable(
+                name: "TestProducts",
+                newName: "TestProducts",
+                newSchema: "satinAlma");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "TestProducts",
+                schema: "satinAlma",
+                newName: "TestProducts");
+        }
+    }
+}
