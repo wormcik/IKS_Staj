d89c777e7bf37200bededbc96e4a6bc00adce8d4
AlpOzgun <154626614+AlpOzgun@users.noreply.github.com>
2024-07-12 15:02:22 +0300

    initial

--- SatinAlim/Entities/User.cs
+++ SatinAlim/Entities/User.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace SatinAlim.Entities
 {
 	public class User
 	{
 
-	}
+		[Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int UserId { get; set; }
+		
+		[Column(TypeName = "VARCHAR(1000)")]
+		public string Name{get;set;}
+
+        [Column(TypeName = "VARCHAR(1000)")]
+		public string LastName { get; set; }
+
+    }
 }
 
