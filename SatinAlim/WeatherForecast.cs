ab81c1e05c3989a3aa5f68faea5bd57c7a1cacd2
Fatih KESİK <fatihkesik@usta.com.tr>
2024-07-12 14:26:42 +0300

    test

--- SatinAlim/WeatherForecast.cs
+++ SatinAlim/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace SatinAlim
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        //asd
         public string? Summary { get; set; }
     }
 }
