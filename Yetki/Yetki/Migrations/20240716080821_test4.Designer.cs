ba4b63c9e9faa9d8d2d372104445c91ad1256a29
AlpOzgun <154626614+AlpOzgun@users.noreply.github.com>
2024-07-16 11:13:29 +0300

    models

--- Yetki/Yetki/Migrations/20240716080821_test4.Designer.cs
+++ Yetki/Yetki/Migrations/20240716080821_test4.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yetki.Entites;
 
@@ -10,9 +11,11 @@ using Yetki.Entites;
 namespace Yetki.Migrations
 {
     [DbContext(typeof(YetkiDbContext))]
-    partial class YetkiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240716080821_test4")]
+    partial class test4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Yetki/Yetki/Migrations/20240716080821_test4.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yetki.Migrations
+{
+    /// <inheritdoc />
+    public partial class test4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "password",
+                schema: "yetki",
+                table: "Users",
+                newName: "Password");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                schema: "yetki",
+                table: "Users",
+                newName: "password");
+        }
+    }
+}
