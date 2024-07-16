f9bbed253dfa39701a4dc20eb848f1ba64007564
Fatih KESİK <fatihkesik@usta.com.tr>
2024-07-16 13:39:42 +0300

    fix

--- Yetki/Yetki/Entites/RegistrationInfo.cs
+++ Yetki/Yetki/Entites/RegistrationInfo.cs
@@ -8,26 +8,16 @@ namespace Yetki.Entites
 {
 	public class RegistrationInfo
 	{
-        
         public Guid? RegistrationUserCode { get; set; }
 
-        
         [Column(TypeName = "VARCHAR(50)")]
         public string? RegistrationUserName { get; set; }
-
-        //[Column(TypeName = "date")]
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
-
-
         public Guid? UpdateUserCode { get; set; }
-
-        //[Column(TypeName = "datetime")]
         public DateTime? UpdatedDate { get; set; }
-
 
         [Column(TypeName = "VARCHAR(50)")]
         public string? UpdateUserName { get; set; }
-
         
         public Guid? ProcessCode { get; set; }
     }
