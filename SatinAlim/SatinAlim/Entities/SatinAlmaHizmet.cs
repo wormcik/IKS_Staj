49af83fccf517b1d3e5a1e88df9b77d10a2eb5ea
Asus <Asus@DESKTOP-20LHBTL>
2024-07-18 14:57:11 +0300

    SatinAlmaHizmet and SatinAlmaUrun

---
+++ SatinAlim/SatinAlim/Entities/SatinAlmaHizmet.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SatinAlim.Entities
+{
+    public class SatinAlmaHizmet
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SatinAlmaHizmetKod { get; set; }
+
+        [Column(TypeName = "VARCHAR(200)")]
+        public string Tanim { get; set; }
+
+        [Column(TypeName = "VARCHAR(2000)")]
+        public string Aciklama { get; set; }
+
+        [Column(TypeName = "VARCHAR(20")]
+        public string Birim { get; set; }
+    }
+}
---
+++ SatinAlim/SatinAlim/Entities/SatinAlmaUrun.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Mime;
+
+namespace SatinAlim.Entities
+{
+    public class SatinAlmaUrun
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int SatinAlmaUrunKod() { get; set; }
+
+        [Column(TypeName = "VARCHAR(200)")]
+        public string Tanim { get; set; }
+
+        [Column(TypeName = "VARCHAR(2000)")]
+        public string Aciklama { get; set; }
+
+        [Column(TypeName = "VARCHAR(20)")]
+        public string Birim { get; set; }
+    }
+}
