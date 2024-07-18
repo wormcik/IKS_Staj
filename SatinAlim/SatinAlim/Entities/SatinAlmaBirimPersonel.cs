5fa0adb97dcf46ca25ad5e84f80f250667ea37d5
Asus <Asus@DESKTOP-20LHBTL>
2024-07-18 13:53:23 +0300

    SatimAlmaBirimPersonel

---
+++ SatinAlim/SatinAlim/Entities/SatinAlmaBirimPersonel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace SatinAlim.Entities
+{
+
+    public class SatinAlmaBirimPersonel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string SatınAlmaBirimPersonelKod { get; set; }
+
+        [ForeignKey("SatınAlmaBirim")]
+        public int SatinAlmaBirimKod { get; set; }
+
+        [ForeignKey("Personel")]
+
+        public int PersonelKod { get; set; }
+
+
+        [JsonIgnore]
+        public Personel? Personel { get; set; }
+
+        [JsonIgnore]
+        public SatinAlmaBirim? SatınAlmaBirim { get; set; }
+
+
+    }
+
+
